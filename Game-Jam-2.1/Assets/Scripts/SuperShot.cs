b201efbb1bd51b865e89a98fdb3001ffe86ccd75
JakeHresh <jakeyhresh123@gmail.com>
2019-10-05 11:20:08 -0700

    Added SFX to shooting magic

--- Game-Jam-2.1/Assets/Scripts/SuperShot.cs
+++ Game-Jam-2.1/Assets/Scripts/SuperShot.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SuperShot : MonoBehaviour
 {
+    public AudioClip SFXClip;
+    public AudioSource SFXSource;
     public GameObject sunSuperProjectile;
     public float speed = 30;
     public Transform firingLocation;
+    void Start()
+    {
+        SFXSource.clip = SFXClip;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -14,6 +20,7 @@ public class SuperShot : MonoBehaviour
             GameObject instantiatedSunProjectile = Instantiate(sunSuperProjectile, firingLocation.position, transform.rotation) as GameObject;
             Rigidbody instBulletRigidbody = instantiatedSunProjectile.GetComponent<Rigidbody>();
             instBulletRigidbody.velocity = transform.TransformDirection(Vector3.forward * speed);
+            SFXSource.Play();
         }
     }
 }
