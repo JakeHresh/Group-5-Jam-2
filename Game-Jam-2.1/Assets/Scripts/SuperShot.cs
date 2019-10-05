49e2d8c67999948803f80f9f96e3effa429529be
JakeHresh <jakeyhresh123@gmail.com>
2019-10-05 09:48:43 -0700

    Implemented Working Shooting Script for Ultimate Shot

--- Game-Jam-2.1/Assets/Scripts/SuperShot.cs
+++ Game-Jam-2.1/Assets/Scripts/SuperShot.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 public class SuperShot : MonoBehaviour
 {
     public GameObject sunSuperProjectile;
-    public float speed = 10;
-
+    public float speed = 30;
+    public Transform firingLocation;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
-            GameObject instantiatedSunProjectile = Instantiate(sunSuperProjectile,
-                                                                transform.position,
-                                                                transform.rotation)
-                                                                as GameObject;
-            Rigidbody instBulletRigidbody = sunSuperProjectile.GetComponent<Rigidbody>();
-            instBulletRigidbody.AddForce(Vector3.forward * speed);
+            GameObject instantiatedSunProjectile = Instantiate(sunSuperProjectile, firingLocation.position, transform.rotation) as GameObject;
+            Rigidbody instBulletRigidbody = instantiatedSunProjectile.GetComponent<Rigidbody>();
+            instBulletRigidbody.velocity = transform.TransformDirection(Vector3.forward * speed);
         }
     }
 }
