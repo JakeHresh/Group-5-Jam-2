a9f7f134b496e6731b0a45484d76fbe89eb01fbe
JakeHresh <jakeyhresh123@gmail.com>
2019-10-04 17:18:33 -0700

    Added super shot script prototype

---
+++ Game-Jam-2.1/Assets/Scripts/SuperShot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SuperShot : MonoBehaviour
+{
+    public GameObject sunSuperProjectile;
+    public float speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject instantiatedSunProjectile = Instantiate(sunSuperProjectile,
+                                                                transform.position,
+                                                                transform.rotation)
+                                                                as GameObject;
+            Rigidbody instBulletRigidbody = sunSuperProjectile.GetComponent<Rigidbody>();
+            instBulletRigidbody.AddForce(Vector3.forward * speed);
+        }
+    }
+}
