49334e96fce058dedd5362b560e0d0b12da7aff4
JakeHresh <jakeyhresh123@gmail.com>
2019-10-05 10:14:58 -0700

    Tweaked the behavior of the super shot projectiles and added invisible walls

---
+++ Game-Jam-2.1/Assets/Scripts/SuperShotBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperShotBehavior : MonoBehaviour
+{
+    public GameObject projectile;
+    public float timer = 10f;
+    private float timeReset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeReset = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            Destroy(projectile);
+        }
+    }
+}
