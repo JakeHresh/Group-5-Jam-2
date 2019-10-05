230cda27bad9281d7c2e48f152fdb4b92a14211e
JakeHresh <jakeyhresh123@gmail.com>
2019-10-05 11:31:24 -0700

    Added a shoot buffer for the super wand attack

--- Game-Jam-2.1/Assets/Scripts/SuperShot.cs
+++ Game-Jam-2.1/Assets/Scripts/SuperShot.cs
@@ -8,6 +8,8 @@ public class SuperShot : MonoBehaviour
     public GameObject sunSuperProjectile;
     public float speed = 30;
     public Transform firingLocation;
+    public float shootBufferTrack = 30f;
+    private float shootBuffer = 0f;
     void Start()
     {
         SFXSource.clip = SFXClip;
@@ -15,12 +17,17 @@ public class SuperShot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && shootBuffer <= 0)
         {
             GameObject instantiatedSunProjectile = Instantiate(sunSuperProjectile, firingLocation.position, transform.rotation) as GameObject;
             Rigidbody instBulletRigidbody = instantiatedSunProjectile.GetComponent<Rigidbody>();
             instBulletRigidbody.velocity = transform.TransformDirection(Vector3.forward * speed);
             SFXSource.Play();
+            shootBuffer = shootBufferTrack;
+        }
+        if(shootBuffer > 0)
+        {
+            shootBuffer--;
         }
     }
 }
