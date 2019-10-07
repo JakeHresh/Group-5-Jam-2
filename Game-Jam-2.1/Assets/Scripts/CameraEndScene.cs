c59d07c10b608073426f755365ab9daad9cd5aa7
rachelh <rachow1@cox.net>
2019-10-06 21:56:21 -0700

    End/Title scenes
    Made end scenes with a simple camera pan and UI instructions. Also a script to return to title/start first level

---
+++ Game-Jam-2.1/Assets/Scripts/CameraEndScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraEndScene : MonoBehaviour
+{
+    public Transform cam;
+    public float speed;
+    public Vector3 move;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = (transform.position + move);
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+
+        }
+    }
+
+    
+}
