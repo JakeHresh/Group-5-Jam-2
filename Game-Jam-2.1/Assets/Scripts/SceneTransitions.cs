c0f8c642c16ed4deafebe19626b7b2f47c4fb26d
JakeHresh <jakeyhresh123@gmail.com>
2019-10-06 22:40:28 -0700

    Implemented Zombie SFX

--- Game-Jam-2.1/Assets/Scripts/SceneTransitions.cs
+++ Game-Jam-2.1/Assets/Scripts/SceneTransitions.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class SceneTransitions : MonoBehaviour
 {
     public GameObject player;
+    public float timer = 120f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,14 @@ public class SceneTransitions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
         if(player == null)
         {
             SceneManager.LoadScene("GameoverScene");
+        }
+        else if(timer <= 0)
+        {
+            SceneManager.LoadScene("VictoryScene");
         }
     }
 }
